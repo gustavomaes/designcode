fc69b7e60eb4411b58abebbf0c19f3dd2caef437
Gustavo <gustavomaescd@gmail.com>
2020-07-17 22:07:06 -0300

    create a array with courses and populate home

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import SplashScreen from 'react-native-splash-screen';
 import Card from './components/Card';
 import { NotificationIcon } from './components/Icons';
 import Logo from './components/Logo';
+import Course from './components/Course';
 
 const Container = styled.View`
   flex: 1;
@@ -106,6 +107,46 @@ const cards = [
     logo: require('../assets/logo-react.png'),
   },
 ];
+
+const courses = [
+  {
+    title: 'Prototype in InVision Studio',
+    subtitle: '10 sections',
+    image: require('../assets/background13.jpg'),
+    logo: require('../assets/logo-studio.png'),
+    author: 'Meng To',
+    avatar: require('../assets/avatar.jpg'),
+    caption: 'Design and interactive prototype',
+  },
+  {
+    title: 'React for Designers',
+    subtitle: '12 sections',
+    image: require('../assets/background11.jpg'),
+    logo: require('../assets/logo-react.png'),
+    author: 'Meng To',
+    avatar: require('../assets/avatar.jpg'),
+    caption: 'Learn to design and code a React site',
+  },
+  {
+    title: 'Design and Code with Framer X',
+    subtitle: '10 sections',
+    image: require('../assets/background14.jpg'),
+    logo: require('../assets/logo-framerx.png'),
+    author: 'Meng To',
+    avatar: require('../assets/avatar.jpg'),
+    caption: 'Create powerful design and code components for your app',
+  },
+  {
+    title: 'Design System in Figma',
+    subtitle: '10 sections',
+    image: require('../assets/background6.jpg'),
+    logo: require('../assets/logo-figma.png'),
+    author: 'Meng To',
+    avatar: require('../assets/avatar.jpg'),
+    caption:
+      'Complete guide to designing a site using a collaborative design tool',
+  },
+];
 const App = () => {
   useEffect(() => {
     SplashScreen.hide();
@@ -154,6 +195,18 @@ const App = () => {
               />
             ))}
           </ScrollView>
+          <Subtitle>Popular coursers</Subtitle>
+          {courses.map((course) => (
+            <Course
+              image={course.image}
+              logo={course.logo}
+              subtitle={course.subtitle}
+              title={course.title}
+              avatar={course.avatar}
+              caption={course.caption}
+              author={course.author}
+            />
+          ))}
         </ScrollView>
       </SafeAreaView>
     </Container>
