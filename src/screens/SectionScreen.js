e5f9b0a61f56009ac81d55ee241805102e1a6aaa
Gustavo <gustavomaescd@gmail.com>
2020-07-22 19:46:50 -0300

    feat: header section

--- src/screens/SectionScreen.js
+++ src/screens/SectionScreen.js
@@ -1,20 +1,107 @@
-import React from 'react';
-import { Button } from 'react-native';
+import React, { useEffect } from 'react';
+import { TouchableOpacity, StatusBar } from 'react-native';
 import styled from 'styled-components';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Container = styled.View`
   flex: 1;
+`;
+
+const Cover = styled.View`
+  height: 375px;
+`;
+
+const Image = styled.Image`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+`;
+
+const Title = styled.Text`
+  font-size: 24px;
+  color: white;
+  font-weight: bold;
+  width: 170px;
+  position: absolute;
+  top: 78;
+  left: 20;
+`;
+
+const Caption = styled.Text`
+  color: white;
+  font-size: 17;
+  position: absolute;
+  bottom: 20;
+  left: 20;
+  width: 200px;
+`;
+
+const CloseView = styled.View`
   justify-content: center;
+  align-items: center;
+  width: 32;
+  height: 32;
+  background-color: white;
+  border-radius: 16;
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+`;
+
+const Wrapper = styled.View`
+  flex-direction: row;
+  position: absolute;
+  top: 40px;
+  left: 20px;
   align-items: center;
 `;
 
-const Text = styled.Text``;
+const Logo = styled.Image`
+  width: 24px;
+  height: 24px;
+`;
 
-const SectionScreen = ({ navigation }) => (
-  <Container>
-    <Text>Section Screen</Text>
-    <Button title="CLOSE" onPress={() => navigation.goBack()} />
-  </Container>
-);
+const Subtitle = styled.Text`
+  font-size: 15;
+  font-weight: 600;
+  color: rgba(255, 255, 255, 0.8);
+  text-transform: uppercase;
+  margin-left: 5px;
+`;
+
+const SectionScreen = ({ route, navigation }) => {
+  const { section } = route.params;
+
+  useEffect(() => {
+    StatusBar.setBarStyle('light-content', true);
+    return () => StatusBar.setBarStyle('dark-content', true);
+  }, []);
+
+  return (
+    <Container>
+      <StatusBar hidden />
+      <Cover>
+        <Image source={section.image} />
+        <Wrapper>
+          <Logo source={section.logo} />
+          <Subtitle>{section.subtitle}</Subtitle>
+        </Wrapper>
+        <Title>{section.title}</Title>
+        <Caption>{section.caption}</Caption>
+      </Cover>
+      <TouchableOpacity
+        style={{ position: 'absolute', top: 20, right: 20 }}
+        onPress={() => navigation.goBack()}
+      >
+        <CloseView>
+          <Ionicons
+            name="ios-close"
+            size={36}
+            color="#4775f2"
+            style={{ marginTop: -2, marginLeft: -2 }}
+          />
+        </CloseView>
+      </TouchableOpacity>
+    </Container>
+  );
+};
 
 export default SectionScreen;
