4b143e8c2cfc902bdea1fc82cd97e2ef3d89c12f
Gustavo <gustavomaescd@gmail.com>
2020-07-21 23:31:46 -0300

    feat: base Section screen

---
+++ src/screens/SectionScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Button } from 'react-native';
+import styled from 'styled-components';
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Text = styled.Text``;
+
+const SectionScreen = ({ navigation }) => (
+  <Container>
+    <Text>Section Screen</Text>
+    <Button title="CLOSE" onPress={() => navigation.goBack()} />
+  </Container>
+);
+
+export default SectionScreen;
