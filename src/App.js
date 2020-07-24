e04f6f1de8354a7507ef365e1217e15b1b6f6301
Gustavo <gustavomaescd@gmail.com>
2020-07-23 22:05:51 -0300

    chore: setup apollo cliente

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import Navigation from './navigator/Navigation';
 
-const App = () => <Navigation />;
+const client = new ApolloClient({
+  uri: 'https://graphql.contentful.com/content/v1/spaces/ldcl3ayg0mhx',
+  credentials: 'same-origin',
+  headers: {
+    Authorization:
+      'Bearer 93f3808c25c1f5bdb95476ca8576c6eaa12b5587efb956efb242ceead7cb3840',
+  },
+  cache: new InMemoryCache(),
+});
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <Navigation />
+  </ApolloProvider>
+);
 
 export default App;
