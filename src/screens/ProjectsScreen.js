129b8d9d0bec09203dce86101a8f9d62b659f150
Gustavo <gustavomaescd@gmail.com>
2020-07-26 19:18:03 -0300

    feat: add one project in ProjectScreen and add PanResponder to make possible move this card

--- src/screens/ProjectsScreen.js
+++ src/screens/ProjectsScreen.js
@@ -1,20 +1,46 @@
-import React from 'react';
-import { Button } from 'react-native';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { PanResponder, Animated } from 'react-native';
+import Project from '../components/Project';
 
 const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
+  background: #f0f3f5;
 `;
 
-const Text = styled.Text``;
+const ProjectsScreen = () => {
+  const [pan] = useState(new Animated.ValueXY());
 
-const ProjectsScreen = ({ navigation }) => (
-  <Container>
-    <Text>ProjectsScreen</Text>
-    <Button title="CLOSE" onPress={() => navigation.goBack()} />
-  </Container>
-);
+  const panResponder = PanResponder.create({
+    onMoveShouldSetPanResponder: () => true,
+    onPanResponderMove: Animated.event([null, { dx: pan.x, dy: pan.y }]),
+    onPanResponderRelease: () => {
+      Animated.spring(pan, {
+        toValue: { x: 0, y: 0 },
+        useNativeDriver: true,
+      }).start();
+    },
+  });
+
+  return (
+    <Container>
+      <Animated.View
+        style={{
+          transform: [{ translateX: pan.x }, { translateY: pan.y }],
+        }}
+        {...panResponder.panHandlers}
+      >
+        <Project
+          title="Price tag"
+          image={require('../../assets/background5.jpg')}
+          author="Gustavo Maes"
+          text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+        />
+      </Animated.View>
+    </Container>
+  );
+};
 
 export default ProjectsScreen;
