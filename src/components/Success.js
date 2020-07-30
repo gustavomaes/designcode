971cdbc87f313bd2116d2c471c0a958ff9a17988
Gustavo <gustavomaescd@gmail.com>
2020-07-30 03:04:08 -0300

    feat: create Success animation with LottieView

---
+++ src/components/Success.js
@@ -0,0 +1,60 @@
+import React, { useRef, useEffect, useState } from 'react';
+import { Animated, Dimensions } from 'react-native';
+import styled from 'styled-components';
+import LottieView from 'lottie-react-native';
+
+const screenHeight = Dimensions.get('window').height;
+
+const Container = styled.View`
+  width: 100%;
+  height: 100%;
+  background: rgba(255, 255, 255, 0.9);
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+const AnimatedContainer = Animated.createAnimatedComponent(Container);
+
+const Success = ({ isActive }) => {
+  const [top] = useState(new Animated.Value(0));
+  const [opacity] = useState(new Animated.Value(0));
+  const animation = useRef(null);
+
+  useEffect(() => {
+    if (isActive) {
+      Animated.timing(top, {
+        toValue: 0,
+        duration: 0,
+      }).start();
+      Animated.timing(opacity, {
+        toValue: 1,
+      }).start();
+      animation.current.play();
+    } else {
+      Animated.timing(top, {
+        toValue: screenHeight,
+        duration: 0,
+      }).start();
+      Animated.timing(opacity, {
+        toValue: 0,
+      }).start();
+      animation.current.loop = false;
+    }
+  }, [isActive]);
+
+  return (
+    <AnimatedContainer style={{ top, opacity }}>
+      <LottieView
+        source={require('../../assets/lottie-checked-done.json')}
+        autoPlay={false}
+        loop={false}
+        ref={animation}
+      />
+    </AnimatedContainer>
+  );
+};
+
+export default Success;
