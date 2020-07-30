8dd138908a49014dd8149abeb7bbb446bcfcbf6d
Gustavo <gustavomaescd@gmail.com>
2020-07-30 16:39:25 -0300

    feat: add loginOpen state in store context

--- src/contexts/StoreContext.js
+++ src/contexts/StoreContext.js
@@ -6,10 +6,20 @@ export const StoreProvider = ({ children }) => {
   const [action, setAction] = useState();
   const [name, setName] = useState();
   const [cardOpen, setCardOpen] = useState(false);
+  const [loginOpen, setLoginOpen] = useState(false);
 
   return (
     <StoreContext.Provider
-      value={{ action, setAction, name, setName, cardOpen, setCardOpen }}
+      value={{
+        action,
+        setAction,
+        name,
+        setName,
+        cardOpen,
+        setCardOpen,
+        loginOpen,
+        setLoginOpen,
+      }}
     >
       {children}
     </StoreContext.Provider>
